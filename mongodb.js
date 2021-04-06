258b20e3f23d2071e4fd82ed0caa3d2d94e0e62e
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-04-06 09:14:33 -0400

    Inserting documents continued

--- mongodb.js
+++ mongodb.js
@@ -15,8 +15,55 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
     
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Maleeha',
-        age: 21
+    // db.collection('users').insertOne({
+    //     name: 'Maleeha',
+    //     age: 21
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.log("Unable to insert user")
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+
+//     db.collection('users').insertMany([
+//         {
+//             name: 'Sadeyah',
+//             age: 19
+//         },
+//         {
+//             name: 'Jose',
+//             age: 24
+//         }
+       
+//     ], (error,result) => {
+//         if(error){
+//             return console.log("Unable to insert users.")
+//         }
+//         console.log(result.ops)
+//     })
+
+    db.collection('tasks').insertMany([
+        {
+            description: "Brush teeth.",
+            completed: true
+        },
+        {
+            description: "Brush hair.",
+            completed: false
+        },
+        {
+            description: "Eat breakfast.",
+            completed: true
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log("Data unable to be inserted")
+        }
+        else {
+            console.log(result.ops)
+        }
     })
-})
+})   
+
