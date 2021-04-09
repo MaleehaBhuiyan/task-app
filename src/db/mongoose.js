3a145efa5a4f880e73d328539feb580de6ed11d3
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-04-09 11:49:43 -0400

    Created Post request for tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,24 +7,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 })
 
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        require: true, 
-        trim: true 
-    }, 
-    completed: {
-        type: Boolean, 
-        default: false 
-    }
-})
 
-const taskOne = new Task({
-    description: "Take a nap.",
-})
-
-taskOne.save().then((taskOne) => {
-    console.log(taskOne)
-}).catch((error) => {
-    console.log(error)
-})
