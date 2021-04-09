66911d8a38d1595fca3fcc04e50cb2163fc83930
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-04-09 11:35:20 -0400

    Created users post request

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 })
-
-const User = mongoose.model('User', {
-    name: { 
-        type: String,
-        required: true,
-        trim: true  
-    }, 
-    age: { 
-        type: Number, 
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error('Age must be a positive number.')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true, 
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid.')
-            }
-        }
-    },
-    password: {
-        type: String, 
-        required: true, 
-        trim: true,
-        minLength: 7,
-        validate(value){
-             if(value.includes('password')){
-                throw new Error('Password cannot contain the word "password".')
-            }
-        }, 
-
-    }
-})
-
-// const me = new User({
-//     name: "Molly ", 
-//     email: "MoLLy123@gmail.com  ",
-//     password: "  himYnAME  "
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log("ERROR", error)
-// })
 
 
 const Task = mongoose.model('Task', {
